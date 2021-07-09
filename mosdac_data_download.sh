4902feec4ecace554cec0a67cabdeaddc3e1a2da
siva sai krishna <sivasai351@gmail.com>
2021-07-09 09:49:52 +0530

    Update mosdac_data_download.sh

--- mosdac_data_download.sh
+++ mosdac_data_download.sh
@@ -1,4 +1,8 @@
-#!/bin/bash                                                                                                                                                              
+#!/bin/bash       
+##upgraded FTP and SFTP service
+$sftp ssk351@download.mosdac.gov.in
+$password:
+
 url=ftp://ftp.mosdac.gov.on/Order
 for i in {75..94..1};do
 echo  wget --user=ssk351 --password=******* $url/Sep19_"0543"${i}"/*"
