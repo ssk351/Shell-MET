5816bd17b6e61aa61567798e76499f87e518c900
Saikrishna <sivasai351@gmail.com>
2022-10-31 23:08:45 +0530

    Update gpcp_data_download.sh

--- gpcp_data_download.sh
+++ gpcp_data_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## To download the GPCP data
 
-for ((ii=2017; ii<=2022; ii++)) do
+for ((ii=2000; ii<=2022; ii++)) do
 
     for ((dd=1; dd<=31; dd++)) do
         if (($dd <= 9)); then
@@ -17,3 +17,5 @@ gpcp-daily/access/"${ii}"/gpcp_v01r03_daily_d"${ii}"01"${dd}"_c20170530.nc"
     done
 
 done
+** note :from 2017 we need to modify "20170530","20171211"
+https://www.ncei.noaa.gov/data/global-precipitation-climatology-project-gpcp-daily/access/2017/gpcp_v01r03_daily_d20170101_c20171211.nc
