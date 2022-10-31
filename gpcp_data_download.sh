9f2bd092a49d47ac6baac5cb8cffaf5865eb6465
Saikrishna <sivasai351@gmail.com>
2022-10-31 23:04:19 +0530

    Download GPCP data via ftp

---
+++ gpcp_data_download.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+## To download the GPCP data
+
+for ((ii=2017; ii<=2022; ii++)) do
+
+    for ((dd=1; dd<=31; dd++)) do
+        if (($dd <= 9)); then
+   wget "https://www.ncei.noaa.gov/data/global-precipitation-climatology-project\
+-\
+gpcp-daily/access/"${ii}"/gpcp_v01r03_daily_d"${ii}"010"${dd}"_c20170530.nc"
+        else
+   wget "https://www.ncei.noaa.gov/data/global-precipitation-climatology-project\
+-\
+gpcp-daily/access/"${ii}"/gpcp_v01r03_daily_d"${ii}"01"${dd}"_c20170530.nc"
+
+        fi
+    done
+
+done
