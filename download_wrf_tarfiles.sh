d7352fc2e273325086322dd0008c44e42d921180
Saikrishna <sivasai351@gmail.com>
2024-02-21 15:11:12 +0530

    To Download WRF tarfiles

---
+++ download_wrf_tarfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#########################################################
+WRFversion="4.3"
+#########################################
+wget https://github.com/wrf-model/WRF/archive/v${WRFversion}.tar.gz
+mv v${WRFversion}.tar.gz WRFV${WRFversion}.tar.gz
+mv v${WRFversion}.tar.gz WRFDAV${WRFversion}.tar.gz
+#########################################
+WPSversion="4.3"
+wget https://github.com/wrf-model/WPS/archive/v${WPSversion}.tar.gz
+mv v${WPSversion}.tar.gz WPSV${WPSversion}.TAR.gz
+
