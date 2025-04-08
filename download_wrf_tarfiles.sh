2ea04199fc78be783f3700a98d9a586d9edaa81b
Saikrishna <sivasai351@gmail.com>
2025-04-08 12:13:54 +0530

    Update download_wrf_tarfiles.sh

--- download_wrf_tarfiles.sh
+++ download_wrf_tarfiles.sh
@@ -2,6 +2,7 @@
 #########################################################
 WRFversion="4.3"
 #########################################
+wget https://github.com/wrf-model/WRF/archive/refs/tags/v${WRFversion}.tar.gz
 wget https://github.com/wrf-model/WRF/archive/v${WRFversion}.tar.gz
 mv v${WRFversion}.tar.gz WRFV${WRFversion}.tar.gz
 mv v${WRFversion}.tar.gz WRFDAV${WRFversion}.tar.gz
